7bbf73077b55bcba7d5a016a9e4173c2517e0d34
adv41 <adv41@cegadv.local>
2026-08-20 13:01:48 -0300

    popup running, added simple stats to main screen from api

--- src/js/app.js
+++ src/js/app.js
@@ -17,11 +17,25 @@ async function chamarApi(city) {
     }
 }
 
+async function currentWeather(lat, lon){
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const resp = await fetch(URL);
+
+    if (resp.status === 200){
+        const obj = await resp.json();
+        return obj;
+    } else {
+        return resp;
+    }
+
+}
+
 async function renderCities(param) {
     let cidades = await param;
     const popup = document.querySelector(".searched-states");
+    popup.replaceChildren();
     popup.classList.add('active-searched');
-    
+
     cidades.forEach(city => {
         const novoItem = document.createElement('div');
         novoItem.classList.add('item');
@@ -34,22 +48,36 @@ async function renderCities(param) {
         popup.appendChild(novoItem);
         novoItem.appendChild(novoButton);
         novoItem.appendChild(novoState);
+
+        novoButton.addEventListener("click", ()=>{
+            renderCity(city.lat, city.lon);
+        })
     })
 
     document.addEventListener('mousedown', (mouse) => {
+        const item = document.querySelectorAll(".item");
+
         if (mouse.button === 0){
 
-            if(!popup.contains(mouse.target)){
+            if(!popup.contains(mouse.target) && !form.contains(mouse.target)){
                 popup.classList.remove('active-searched');
+                popup.replaceChildren();
             }
         }
     }) 
         
 }
 
-function ocultarRenderCities(){
-    const popup = document.querySelector(".searched-states");
-    
+async function renderCity(lat, lon){
+    const weatherInformations = await currentWeather(lat, lon);
+    const screenResult = document.querySelector(".search-result");
+    console.log(weatherInformations);
+
+    const degreeNow = screenResult.querySelector(".degree-now");
+    const description = screenResult.querySelector(".description");
+
+    degreeNow.textContent = Math.round(weatherInformations.main.temp - 273.15);
+    description.textContent = weatherInformations.weather[0].description;
 }
 
 form.addEventListener('submit', (evento) => {
