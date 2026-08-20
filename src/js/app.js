68c818df695d5fdb67f85a6ff1fb4909755f63f2
adv41 <adv41@cegadv.local>
2026-08-20 17:55:52 -0300

    added history

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
+
 const form = document.getElementById('search');
 const searchInput = document.getElementById('searchInput');
 let cities = "";
 const API_KEY = '6ff8333827614515dcfb3b0c92d52298';
 
+let history = {}
 
 async function chamarApi(city) {
 
@@ -51,14 +53,37 @@ async function renderCities(param) {
 
         novoButton.addEventListener("click", ()=>{
             renderCity(city.lat, city.lon);
+            renderHistory(city);
         })
     })
 
+    async function getWeatherDays(lat, lon){
+        const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+        
+        const resp = await fetch(URL);
+        if(resp === 200){
+            const obj = await resp.json();
+            return obj;
+        } else {
+            return null;
+        }
+
+    }
+
+    async function separeWeatherDays(lat, lon){
+        const days = await getWeatherDays(lat, lon);
+        return days;
+    }
+
+    console.log(separeWeatherDays(-44.4667342, -44.0289169));
+
+    //Listener novo: mousedown (click)
     document.addEventListener('mousedown', (mouse) => {
         const item = document.querySelectorAll(".item");
 
         if (mouse.button === 0){
 
+            //metodo novo: contains
             if(!popup.contains(mouse.target) && !form.contains(mouse.target)){
                 popup.classList.remove('active-searched');
                 popup.replaceChildren();
@@ -71,13 +96,55 @@ async function renderCities(param) {
 async function renderCity(lat, lon){
     const weatherInformations = await currentWeather(lat, lon);
     const screenResult = document.querySelector(".search-result");
-    console.log(weatherInformations);
 
     const degreeNow = screenResult.querySelector(".degree-now");
     const description = screenResult.querySelector(".description");
 
     degreeNow.textContent = Math.round(weatherInformations.main.temp - 273.15);
     description.textContent = weatherInformations.weather[0].description;
+}
+
+function addHistory(city, state, lat, lon){
+    //Método novo
+    let nextId = Object.keys(history).length + 1;
+
+    history[nextId] = {
+        city: city, 
+        state: state, 
+        lat: lat, 
+        lon: lon
+    }
+
+}
+
+function renderHistory(city){
+    
+    addHistory(city.name, city.state, city.lat, city.lon);
+    const popup = document.querySelector(".history");
+    popup.replaceChildren();
+
+    for (let id in history) {
+
+        const novoItem = document.createElement("button")
+        novoItem.classList.add("history-item");
+
+        const cityName = document.createElement("p");
+        cityName.textContent = history[id].city;
+
+        const stateName = document.createElement("p");
+        stateName.textContent = history[id].state;
+
+
+        popup.appendChild(novoItem);
+        novoItem.appendChild(cityName);
+        novoItem.appendChild(stateName);
+
+        novoItem.addEventListener("click", ()=>{
+            renderCity(history[id].lat, history[id].lon);
+        })
+
+    }
+
 }
 
 form.addEventListener('submit', (evento) => {
@@ -88,6 +155,7 @@ form.addEventListener('submit', (evento) => {
     const dados = new FormData(evento.target);
     const cidade = dados.get('city');
 
+});
 
-})
+
 
